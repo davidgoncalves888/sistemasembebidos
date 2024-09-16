e2ba9f7070c95f277cb4c1eb0207d4a2cbd541a5
davidgoncalves888 <david.goncalves.alvarez@udc.es>
2024-09-16 17:35:15 +0200

    Led debería encender

--- main.c
+++ main.c
@@ -4,49 +4,43 @@
 // LED_GREEN = PTD5
 // LED_RED = PTE29
 
-void delay(void)
-{
-  volatile int i;
+void delay(void) {
+    volatile int i;
 
-  for (i = 0; i < 1000000; i++);
+    for (i = 0; i < 1000000; i++);
 }
 
 // LED_GREEN = PTD5
-void led_green_init()
-{
-  // SIM->COPC
-  // SIM->SCGC5
-  // PORTD->PCR[5]
-  // GPIOD->PDDR
-  // GPIOD->PSOR
+void led_green_init() {
+    SIM->COPC = 0;
+    SIM->SCGC5 |= 0x1000u;
+    PORTD->PCR[5] |= 0x100u;
+    GPIOD->PDDR |= 0x20u;
+    GPIOD->PSOR;;
 }
 
-void led_green_toggle()
-{
-  //
+void led_green_toggle() {
+    //
 }
 
 // LED_RED = PTE29
-void led_red_init()
-{
-  //
+void led_red_init() {
+    //
 }
 
-void led_red_toggle(void)
-{
-  //
+void led_red_toggle(void) {
+    //
 }
 
-int main(void)
-{
-  led_green_init();
-  led_red_init();
+int main(void) {
+    led_green_init();
+    led_red_init();
 
-  while (1) {
-    led_green_toggle();
-    delay();
-    led_red_toggle();
-  }
+    while (1) {
+        led_green_toggle();
+        delay();
+        led_red_toggle();
+    }
 
-  return 0;
+    return 0;
 }
