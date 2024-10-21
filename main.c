b8ccc60bf93c72c4c1ef2cfe38d8867380c5b515
davidgoncalves888 <david.goncalves.alvarez@udc.es>
2024-10-21 15:01:35 +0200

    Práctica 3 interrupciones casi

--- main.c
+++ main.c
@@ -2,23 +2,23 @@
 #include "MKL46Z4.h"
 #include "lcd.h"
 
-// LED (RG)
-#define LED_GREEN PTD5 (pin 98)
-#define LED_RED PTE29 (pin 26)
+#define SW1_PIN 3
+#define SW3_PIN 12
 
-// SWICH
-// RIGHT (SW1) = PTC3 (pin 73)
-// LEFT (SW2) = PTC12 (pin 88)
 
 // Enable IRCLK (Internal Reference Clock)
-// see Chapter 24 in MCU doc
 void irclk_ini() {
     MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
-    MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
+    MCG->C2 = MCG_C2_IRCS(0); // 0 = 32KHZ internal reference clock; 1 = 4MHz irc
 }
 
 void disable_watchdog() {
     SIM->COPC = 0;
+}
+
+void delay(void) {
+    volatile int i;
+    for (i = 0; i < 3000000; i++);
 }
 
 void led_green_init() {
@@ -59,96 +59,106 @@ void led_red_off() {
     GPIOE->PSOR |= 1 << 29;
 }
 
-void switch1_init() {
-    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-    PORTC->PCR[3] |= PORT_PCR_PS(1) | PORT_PCR_PE(1) | PORT_PCR_MUX(1);
-    GPIOC->PDDR &= ~(1 << 3);
-}
 
-int sw1_is_pressed(void) {
-    return (!(GPIOC->PDIR & (1 << 3)));
+/*void switch1_init() {
+    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+    PORTC->PCR[3] |= PORT_PCR_PS(1) | PORT_PCR_PE(1) | PORT_PCR_MUX(1) | PORT_PCR_IRQC(0b1010) | PORT_PCR_ISF(1);  // Interrupt on falling edge
+    GPIOC->PDDR &= ~(1 << 3);
+    NVIC_EnableIRQ(PORTC_PORTD_IRQn);  // Enable PORTC interrupt in NVIC
+}*/
+void switch_init() {
+    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+
+    /* Configure pin as GPIO */
+    PORTC->PCR[SW1_PIN] |= PORT_PCR_MUX(1);
+    PORTC->PCR[SW3_PIN] |= PORT_PCR_MUX(1);
+    /* Enable internal pullup resistor */
+    PORTC->PCR[SW1_PIN] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+    PORTC->PCR[SW3_PIN] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+    /* Interrupt on falling edge */
+    PORTC->PCR[SW1_PIN] |= PORT_PCR_IRQC(0x0A);
+    PORTC->PCR[SW3_PIN] |= PORT_PCR_IRQC(0x0A);
+
+    /* Enable IRQ */
+    NVIC_ClearPendingIRQ(PORTC_PORTD_IRQn);
+    NVIC_EnableIRQ(PORTC_PORTD_IRQn);
+    NVIC_SetPriority(PORTC_PORTD_IRQn, 1);
 }
 
 void switch3_init() {
     SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-    PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PS_MASK | PORT_PCR_PE_MASK;
+    PORTC->PCR[12] |= PORT_PCR_PS(1) | PORT_PCR_PE(1) | PORT_PCR_MUX(1) | PORT_PCR_IRQC(0b1010) |
+                      PORT_PCR_ISF(1);  // Interrupt on falling edge
     GPIOC->PDDR &= ~(1 << 12);
+    NVIC_EnableIRQ(PORTC_PORTD_IRQn);  // Enable PORTC interrupt in NVIC
 }
 
-int sw3_is_pressed(void) {
-    return !(GPIOC->PDIR & (1 << 12));
-}
+volatile int hit = 0;
+volatile int misses = 0;
+volatile bool stop_loop = false;
 
-void delay(void) {
-    volatile int i;
 
-    for (i = 0; i < 3000000; i++);
+// Interrupt Service Routine for PORTC and PORTD
+void PORTC_PORTD_IRQHandler(void) {
+    if (PORTC->PCR[SW1_PIN] & PORT_PCR_ISF_MASK) {  // SW1 pressed (Right)
+        hit++;  // Increment hit counter
+        stop_loop = true;
+        PORTC->PCR[SW1_PIN] |= PORT_PCR_ISF_MASK;
+    }
+    if (PORTC->PCR[SW3_PIN] & PORT_PCR_ISF_MASK) {  // SW3 pressed (Left)
+        misses++;  // Increment misses counter
+        stop_loop = true;
+        PORTC->PCR[SW3_PIN] |= PORT_PCR_ISF_MASK;
+    }
 }
 
 int main(void) {
     disable_watchdog();
     led_green_init();
     led_red_init();
-    switch1_init();
-    switch3_init();
+    switch_init();
     irclk_ini();
     lcd_ini();
 
-    int hit = 0, misses = 0;
-    bool stop_loop = false;
     lcd_display_dec(666);
     lcd_display_time(12, 24);
 
-    // 'Random' sequence :-)
-    volatile unsigned int sequence = 0x32B14D98,
-            index = 0;
+    volatile unsigned int sequence = 0x32B14D98;
+    unsigned int index = 0;
 
     while (index < 32) {
-        if (sequence & (1 << index)) { //derecho
+        stop_loop = false;  // Reset the loop control
+
+        if (sequence & (1 << index)) {  // Green LED (Right)
             led_green_on();
             while (!stop_loop) {
-                if (sw1_is_pressed()) {
-                    hit++;
-                    stop_loop = true;
-                    while (sw1_is_pressed());
-                } else if (sw3_is_pressed()) {
-                    misses++;
-                    stop_loop = true;
-                    while (sw3_is_pressed());
-                }
+                // Wait for an interrupt (handled in ISR)
                 lcd_display_time(hit, misses);
             }
-            stop_loop = false;
             led_green_off();
-        } else { //izquierdo
+        } else {  // Red LED (Left)
             led_red_on();
             while (!stop_loop) {
-                if (sw1_is_pressed()) {
-                    misses++;
-                    stop_loop = true;
-                    while (sw1_is_pressed());
-                } else if (sw3_is_pressed()) {
-                    hit++;
-                    stop_loop = true;
-                    while (sw3_is_pressed());
-                }
+                // Wait for an interrupt (handled in ISR)
                 lcd_display_time(hit, misses);
             }
-            stop_loop = false;
             led_red_off();
         }
-        index++;
+
+        index++;  // Move to the next step in the sequence
     }
+
+    // Flash the final result (hits and misses)
     int i = 20;
-    while (i != 0) {
+    while (i > 0) {
         i--;
         lcd_display_time(hit, misses);
         delay();
         lcd_display_time(0, 0);
         delay();
     }
+
     while (1) {
     }
-
     return 0;
 }
--- makefile
+++ makefile
@@ -13,8 +13,8 @@ OBJ_MAIN = $(SRC_MAIN:%.c=%.o)
 INCLUDES = -I ./includes -I ./drivers
 CPU = -DCPU_MKL46Z256VLL4
 
-CFLAGS = -O2 -Wall -mthumb -mcpu=cortex-m0plus $(INCLUDES) $(CPU) -c
-LDFLAGS = -O2 -Wall -mthumb -mcpu=cortex-m0plus -specs=nano.specs -Wl,--gc-sections,
+CFLAGS = -O0 -g3 -Wall -mthumb -mcpu=cortex-m0plus $(INCLUDES) $(CPU) -c
+LDFLAGS = -O0 -g3  -Wall -mthumb -mcpu=cortex-m0plus -specs=nano.specs -Wl,--gc-sections,
 
 OPENOCD_CMD = openocd -f openocd.cfg -c "program main.elf verify reset exit"
 
@@ -28,4 +28,4 @@ flash_main: $(TARGET_MAIN)
 	openocd -f openocd.cfg -c "program $< verify reset exit"
 
 clean:
-	rm -f $(OBJ_LED) $(OBJ_HELLO) $(TARGET_LED) $(TARGET_HELLO) $(TARGET_LED:.elf=.map) $(TARGET_HELLO:.elf=.map)
+	rm -f $(OBJ_MAIN) $(OBJ_MAIN) $(TARGET_MAIN) $(TARGET_MAIN) $(TARGET_MAIN:.elf=.map)
