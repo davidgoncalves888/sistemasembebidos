d3e7d32cc402a82ddfaad80977379479c34e7550
davidgoncalves888 <david.goncalves.alvarez@udc.es>
2024-12-02 16:52:35 +0100

    Practica 5

--- main.c
+++ main.c
@@ -1,54 +1,62 @@
-#include <stdbool.h>
+#include <stdlib.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 #include "MKL46Z4.h"
 #include "lcd.h"
 
+#define QUEUE_LENGTH 99
+#define QUEUE_ITEM_SIZE sizeof(int)
 #define SW1_NUM 3 // Derecha
-#define SW3_NUM 12 // Left
+#define SW3_NUM 12 // Izquierda
 
-
-// Enable IRCLK (Internal Reference Clock)
-void irclk_ini() {
-    MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
-    MCG->C2 = MCG_C2_IRCS(0); // 0 = 32KHZ internal reference clock; 1 = 4MHz irc
-}
+QueueHandle_t messageQueue;
+volatile int numProducers = 0;
+volatile int numConsumers = 0;
 
 void disable_watchdog() {
     SIM->COPC = 0;
 }
 
-void delay(int delayTime) {
-    volatile int i;
-    for (i = 0; i < delayTime; i++);
+void irclk_ini() {
+    MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
+    MCG->C2 = MCG_C2_IRCS(0); // 0 = 32KHZ internal reference clock; 1 = 4MHz irc
 }
 
-void led_green_init() {
-    SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-    PORTD->PCR[5] |= PORT_PCR_MUX(1);
-    GPIOD->PDDR |= 0x20u;
-    GPIOD->PSOR |= 1 << 5;
+void updateLCD(int pendingMessages) {
+    lcd_display_time(numProducers * 10 + numConsumers, pendingMessages);
 }
 
-void led_green_on() {
-    GPIOD->PCOR |= 1 << 5;
+// Productor de datos
+void producerTask(void *pvParameters) {
+    int data;
+    while (1) {
+        if (numProducers > 0) {
+            data = rand() % 100; // Generar datos aleatorios
+            if (xQueueSend(messageQueue, &data, portMAX_DELAY) == pdPASS) {
+                updateLCD(uxQueueMessagesWaiting(messageQueue));
+            }
+            vTaskDelay(pdMS_TO_TICKS(1000 / numProducers)); // Delay en función de la cantidad de productores
+        }
+    }
 }
 
-void led_green_off() {
-    GPIOD->PSOR |= 1 << 5;
+// Consumidor de datos
+void consumerTask(void *pvParameters) {
+    int data;
+    while (1) {
+        if (numConsumers > 0) {
+            if (xQueueReceive(messageQueue, &data, portMAX_DELAY) == pdPASS) {
+                updateLCD(uxQueueMessagesWaiting(messageQueue));
+            }
+            vTaskDelay(pdMS_TO_TICKS(1000 / numConsumers)); // Delay en función de la cantidad de consumidores
+        }
+    }
 }
 
-void led_red_init() {
-    SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-    PORTE->PCR[29] |= PORT_PCR_MUX(1);
-    GPIOE->PDDR |= 1 << 29;
-    GPIOE->PSOR |= 1 << 29;
-}
-
-void led_red_on() {
-    GPIOE->PCOR |= 1 << 29;
-}
-
-void led_red_off() {
-    GPIOE->PSOR |= 1 << 29;
+void adjustProducersConsumers() {
+    numProducers = 4;
+    numConsumers = 1;
 }
 
 void switch_init() {
@@ -73,80 +81,40 @@ void switch_init() {
     NVIC_SetPriority(PORTC_PORTD_IRQn, 1);
 }
 
-volatile int hit = 0;
-volatile int misses = 0;
-volatile bool stop_loop = false;
-volatile bool sw1_pressed = false, sw3_pressed = false;
-
-
-// Rutina de interrupciones para el puerto D
 void PORTDIntHandler(void) {
     if (PORTC->PCR[SW1_NUM] & PORT_PCR_ISF_MASK) {  // SW1 pressed (Right)
-        stop_loop = true;
-        sw1_pressed = true;
+        numConsumers >= 4 ? numConsumers = 0 : numConsumers++;
         PORTC->PCR[SW1_NUM] |= PORT_PCR_ISF_MASK;
     }
     if (PORTC->PCR[SW3_NUM] & PORT_PCR_ISF_MASK) {  // SW3 pressed (Left)
-        stop_loop = true;
-        sw3_pressed = true;
+        numProducers >= 4 ? numProducers = 0 : numProducers++;
         PORTC->PCR[SW3_NUM] |= PORT_PCR_ISF_MASK;
     }
 }
 
-int main(void) {
+int main() {
     disable_watchdog();
-    led_green_init();
-    led_red_init();
-    switch_init();
     irclk_ini();
     lcd_ini();
-
-    volatile unsigned int sequence = 0x32B14D98;
-    unsigned int index = 0;
-
+    switch_init();
     lcd_display_time(0, 0);
-    while (index < 32) {
-        stop_loop = false;
-
-        if (sequence & (1 << index)) {  // LED verde (Derecha)
-            led_green_on();
-            while (!stop_loop) {}
-            if (sw1_pressed) {
-                hit++;
-                sw1_pressed = false;
-            } else if (sw3_pressed) {
-                misses++;
-                sw3_pressed = false;
-            }
-
-            led_green_off();
-        } else {  // LED rojo (Izquierda)
-            led_red_on();
-            while (!stop_loop) {}
-            if (sw1_pressed) {
-                misses++;
-                sw1_pressed = false;
-            } else if (sw3_pressed) {
-                hit++;
-                sw3_pressed = false;
-            }
-            led_red_off();
-        }
-        lcd_display_time(hit, misses);
-        index++;
+    messageQueue = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
+    if (messageQueue == NULL) {
+        lcd_display_time(1, 1);
+        return 1;
     }
 
+    // Crear tareas de productor y consumidor
+    xTaskCreate(producerTask, "ProducerTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    xTaskCreate(consumerTask, "ConsumerTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
-    int i = 20;
-    while (i > 0) {
-        i--;
-        lcd_display_time(hit, misses);
-        delay(2000000);
-        lcd_display_time(0, 0);
-        delay(2000000);
-    }
+    // Simular cambio en productores y consumidores
+    adjustProducersConsumers();
 
-    while (1) {
-    }
+    // Iniciar el scheduler
+    vTaskStartScheduler();
+
+    // Este punto no debería alcanzarse
+    for (;;);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,57 +1,32 @@
 CC = arm-none-eabi-gcc
 
-# Targets for each compilation
-TARGET_EMBEBIDO = emb.elf
-TARGET_ENSAMBLAR = ens.elf
+TARGET_MAIN = main.elf
 
-# Common source files
-COMMON_SRC = startup.c main.c
+COMMON_SRC = startup.c \
+             $(wildcard ./drivers/*.c) \
+             $(wildcard ./FreeRTOS/*.c)
 
-# Source files and objects for each driver version
-SRC_EMBEBIDO = $(COMMON_SRC) $(wildcard ./drivers_EMB/*.c)
-SRC_ENSAMBLAR = $(COMMON_SRC) $(wildcard ./drivers_ENS/*.c)
+SRC_MAIN = $(COMMON_SRC) \
+          main.c
 
-OBJ_EMBEBIDO = $(SRC_EMBEBIDO:%.c=%.o)
-OBJ_ENSAMBLAR = $(SRC_ENSAMBLAR:%.c=%.o) ./drivers_ENS/My_Div2.o
+OBJ_MAIN = $(SRC_MAIN:%.c=%.o)
 
-# Includes for each driver version
-INCLUDES = -I ./includes
-INCLUDES_EMB = -I ./includes -I ./drivers_EMB
-INCLUDES_ENS = -I ./includes -I ./drivers_ENS
-
-# Common compilation flags
+INCLUDES = -I ./includes -I ./include -I ./FreeRTOS
 CPU = -DCPU_MKL46Z256VLL4
-CFLAGS = -O0 -g3 -Wall -mthumb -mcpu=cortex-m0plus $(CPU) -c
-LDFLAGS = -O0 -g3 -Wall -mthumb -mcpu=cortex-m0plus -specs=nano.specs -Wl,--gc-sections,
 
-# Rules for generating .o files for each driver version
+CFLAGS = -O0 -g3 -Wall -mthumb -mcpu=cortex-m0plus $(INCLUDES) $(CPU) -c
+LDFLAGS = -O0 -g3  -Wall -mthumb -mcpu=cortex-m0plus -specs=nosys.specs -Wl,--gc-sections,
+
+OPENOCD_CMD = openocd -f openocd.cfg -c "program main.elf verify reset exit"
+
 %.o: %.c
-	$(CC) $(CFLAGS) $(INCLUDES) -o $@ $<
+	$(CC) $(CFLAGS) -o $@ $<
 
-drivers_EMB/%.o: drivers_EMB/%.c
-	$(CC) $(CFLAGS) $(INCLUDES_EMB) -o $@ $<
+$(TARGET_MAIN): $(OBJ_MAIN)
+	$(CC) $(LDFLAGS)-Map=$(@:.elf=.map) -Tlink.ld $(OBJ_MAIN) -o $(TARGET_MAIN)
 
-drivers_ENS/%.o: drivers_ENS/%.c
-	$(CC) $(CFLAGS) $(INCLUDES_ENS) -o $@ $<
-
-# Rule to assemble My_Div2.s to My_Div2.o
-./drivers_ENS/My_Div2.o: ./drivers_ENS/My_Div2.s
-	arm-none-eabi-as -mthumb -mcpu=cortex-m0plus -o $@ $<
-
-# Linking rules for each target
-$(TARGET_EMBEBIDO): $(OBJ_EMBEBIDO)
-	$(CC) $(LDFLAGS)-Map=$(@:.elf=.map) -Tlink.ld $(OBJ_EMBEBIDO) -o $(TARGET_EMBEBIDO)
-
-$(TARGET_ENSAMBLAR): $(OBJ_ENSAMBLAR)
-	$(CC) $(LDFLAGS)-Map=$(@:.elf=.map) -Tlink.ld $(OBJ_ENSAMBLAR) -o $(TARGET_ENSAMBLAR)
-
-# Flash commands for each target
-flash_embebido: $(TARGET_EMBEBIDO)
+flash_main: $(TARGET_MAIN)
 	openocd -f openocd.cfg -c "program $< verify reset exit"
 
-flash_ensamblar: $(TARGET_ENSAMBLAR)
-	openocd -f openocd.cfg -c "program $< verify reset exit"
-
-# Clean up objects and targets
 clean:
-	rm -f $(OBJ_EMBEBIDO) $(OBJ_ENSAMBLAR) $(TARGET_EMBEBIDO) $(TARGET_ENSAMBLAR) $(TARGET_EMBEBIDO:.elf=.map) $(TARGET_ENSAMBLAR:.elf=.map)
+	rm -f $(OBJ_MAIN) $(OBJ_MAIN) $(TARGET_MAIN) $(TARGET_MAIN) $(TARGET_MAIN:.elf=.map)
