92978cde0ad2a05016f85b03d56c5c817083f769
davidgoncalves888 <david.goncalves.alvarez@udc.es>
2024-12-30 20:13:31 +0100

    TGR 2 fin

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TGR 2 - SE 24/25
 
-Se ha decidido usar el sensor de luz 
+Se ha decidido usar el sensor de luz.
+Los dos leds se encienden al máximo con poca luz y se quedan a poca potencia con mucha luz.
+El sensor de luz es muy poco sensible, hay que acercar el flash del móvil justo encima para que cambien los leds.
--- main.c
+++ main.c
@@ -1,12 +1,5 @@
-#include <stdbool.h>
 #include "MKL46Z4.h"
 #include "lcd.h"
-
-#define LIGHT_SENSOR_ADC_CHANNEL  (8U)
-
-#define SW1_NUM 3 // Derecha
-#define SW3_NUM 12 // Izquierda
-
 
 void disable_watchdog() {
     SIM->COPC = 0;
@@ -17,105 +10,74 @@ void irclk_ini() {
     MCG->C2 = MCG_C2_IRCS(0); // 0 = 32KHZ internal reference clock; 1 = 4MHz irc
 }
 
-void led_green_init() {
-    SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-    PORTD->PCR[5] |= PORT_PCR_MUX(1);
-    GPIOD->PDDR |= 0x20u;
-    GPIOD->PSOR |= 1 << 5;
-}
-
-void led_green_on() {
-    GPIOD->PCOR |= 1 << 5;
-}
-
-void led_green_off() {
-    GPIOD->PSOR |= 1 << 5;
-}
-
-void led_red_init() {
-    SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-    PORTE->PCR[29] |= PORT_PCR_MUX(1);
-    GPIOE->PDDR |= 1 << 29;
-    GPIOE->PSOR |= 1 << 29;
-}
-
-void led_red_on() {
-    GPIOE->PCOR |= 1 << 29;
-}
-
-void led_red_off() {
-    GPIOE->PSOR |= 1 << 29;
-}
-
-void flash_leds() {
-    bool static ledsOn = false;
-    if (ledsOn) {
-        led_green_off();
-        led_red_off();
-        ledsOn = false;
-    } else {
-        led_green_on();
-        led_red_on();
-        ledsOn = true;
-    }
-}
-
-void switch_init() {
-    //Se activa el reloj para el puerto C
-    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-
-    //Se configura el pin como GPIO
-    PORTC->PCR[SW1_NUM] |= PORT_PCR_MUX(1);
-    PORTC->PCR[SW3_NUM] |= PORT_PCR_MUX(1);
-
-    //Se activa la resistencia
-    PORTC->PCR[SW1_NUM] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-    PORTC->PCR[SW3_NUM] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
-
-    //Interrupciones cuando baje el flanco
-    PORTC->PCR[SW1_NUM] |= PORT_PCR_IRQC(0b1010);
-    PORTC->PCR[SW3_NUM] |= PORT_PCR_IRQC(0b1010);
-
-    //Activar Interrupciones
-    NVIC_ClearPendingIRQ(PORTC_PORTD_IRQn);
-    NVIC_EnableIRQ(PORTC_PORTD_IRQn);
-    NVIC_SetPriority(PORTC_PORTD_IRQn, 1);
-}
-
-
 void ADC_Init(void) {
+    // Enable the clock to ADC0
     SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
 
-    ADC0->CFG1 = ADC_CFG1_ADICLK(0) |
-                 ADC_CFG1_MODE(1) |
-                 ADC_CFG1_ADIV(2);
+    // Configure ADC0
+    ADC0->CFG1 = ADC_CFG1_ADICLK(0) | // Bus clock source
+                 ADC_CFG1_MODE(1) |   // 12-bit conversion
+                 ADC_CFG1_ADIV(2);    // Divide input clock by 4
 
-    ADC0->SC1[0] = ADC_SC1_ADCH(31);
+    ADC0->SC1[0] = ADC_SC1_ADCH(31);  // Disable module (default state)
 }
 
 uint16_t ADC_Read(uint8_t channel) {
+    // Selección de canal
     ADC0->SC1[0] = channel;
+    // Esperar hasta que acabe la conversión
     while (!(ADC0->SC1[0] & ADC_SC1_COCO_MASK));
     return ADC0->R[0];
 }
 
 
+void PWM_Init(void) {
+    // Reloj
+    SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;
+    SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK;
+
+    // Configurar puertos
+    PORTD->PCR[5] = PORT_PCR_MUX(4);
+    PORTE->PCR[29] = PORT_PCR_MUX(3);
+
+    // Fuente reloj
+    SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
+    // TPM0 a PWM
+    TPM0->SC = TPM_SC_CMOD(1) | TPM_SC_PS(4);
+    TPM0->MOD = 255;
+
+    // TPM0_CH5 para el LED verde
+    TPM0->CONTROLS[5].CnSC = TPM_CnSC_MSB_MASK | TPM_CnSC_ELSB_MASK;
+    TPM0->CONTROLS[5].CnV = 0;
+
+    // TPM0_CH2 para el LED rojo
+    TPM0->CONTROLS[2].CnSC = TPM_CnSC_MSB_MASK | TPM_CnSC_ELSB_MASK;
+    TPM0->CONTROLS[2].CnV = 0;
+}
+
+void PWM_SetDutyCycle(TPM_Type *tpm, uint8_t channel, uint8_t duty_cycle) {
+    if (duty_cycle > 100) duty_cycle = 100;
+    tpm->CONTROLS[channel].CnV = (tpm->MOD * duty_cycle) / 100;
+}
+
+
 int main(void) {
     uint16_t light_value;
-    int light_reference;
+    uint8_t green_duty_cycle, red_duty_cycle;
     disable_watchdog();
     irclk_ini();
     lcd_ini();
     lcd_display_time(0, 0);
-
     ADC_Init();
-    light_reference = ADC_Read(3);
+    PWM_Init();
 
     while (1) {
         light_value = ADC_Read(3);
         lcd_display_dec(light_value);
-
-
+        green_duty_cycle = 100 - (light_value * 100 / 4095);
+        red_duty_cycle = 100 - (light_value * 100 / 4095);
+        PWM_SetDutyCycle(TPM0, 5, green_duty_cycle);
+        PWM_SetDutyCycle(TPM0, 2, red_duty_cycle);
         for (volatile int i = 0; i < 1000000; i++);
     }
 }
